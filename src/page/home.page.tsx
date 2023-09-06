7efb6bc9049fe4d44bccacbafba81584e401aea3
Misho <chapidze.misho95@gmail.com>
2023-09-06 11:36:26 +0400

    sss

--- src/page/home.page.tsx
+++ src/page/home.page.tsx
@@ -1,11 +1,13 @@
 import CategoryList from "../components/home/category.list";
 import HomeHeader from "../components/home/home.header";
+import ProductComponent from "../components/home/product.component";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-5">
       <HomeHeader />
       <CategoryList />
+      <ProductComponent />
     </div>
   );
 };
